0becd8393e9ee4e4c8ee69975cf149b119f3e91c
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-05-01 10:49:59 +0100

    modify API - create dummy data for courses - create actions/resolvers to retrieve specific data - update server.js

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const express_graphql = require('express-graphql');
 const { buildSchema } = require('graphql');
+const { getCourse, getCourses } = require('./resolvers');
 
 // GraphQL schema
 const schema = buildSchema(`
   type Query {
-    message: String
+    course(id: Int!): Course
+    courses(topic: String): [Course]
+  },
+  type Course {
+    id: Int
+    title: String
+    author: String
+    description: String
+    topic: String
+    url: String
   }
 `);
 
 // Root resolver
 const root = {
-    message: () => 'Hello World!'
+  course: getCourse,
+  courses: getCourses
 };
 
 // Create an express server and a GraphQL endpoint
